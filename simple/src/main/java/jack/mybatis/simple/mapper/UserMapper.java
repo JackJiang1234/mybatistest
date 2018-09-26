5f391d65cc835b32158b21955f57fe995d13f93e
Jack <jjy_8888@163.com>
2018-09-26 17:29:42 +0800

    insert,update,delete test

--- simple/src/main/java/jack/mybatis/simple/mapper/UserMapper.java
+++ simple/src/main/java/jack/mybatis/simple/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package jack.mybatis.simple.mapper;
 
 import jack.mybatis.simple.model.SysUser;
 import jack.mybatis.simple.model.SysRole;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -11,13 +12,13 @@ public interface UserMapper {
      *
      * @param id
      * @return
-     * */
+     */
 
     SysUser selectById(Long id);
 
     /**
      * get all users
-     * */
+     */
     List<SysUser> selectAll();
 
     /**
@@ -25,6 +26,49 @@ public interface UserMapper {
      *
      * @param userid
      * @return
-     * */
+     */
     List<SysRole> selectRolesByUserId(Long userid);
+
+    /**
+     * @param sysUser
+     * @return
+     */
+    int insert(SysUser sysUser);
+
+    /**
+     * @param sysUser
+     * @return
+     */
+    int insert2(SysUser sysUser);
+
+    /**
+     * @param sysUser
+     * @return
+     */
+    int insert3(SysUser sysUser);
+
+    /**
+     * update by key
+     *
+     * @param sysUser
+     * @return
+     * */
+    int updateById(SysUser sysUser);
+
+    /**
+     * 通过主键删除
+     *
+     * @param id
+     * @return
+     */
+    int deleteById(SysUser sysUser);
+
+    /**
+     * 根据用户 id 和 角色的 enabled 状态获取用户的角色
+     *
+     * @param userId
+     * @param enabled
+     * @return
+     */
+    List<SysRole> selectRolesByUserIdAndRoleEnabled(@Param("userId")Long userId, @Param("enabled")Integer enabled);
 }
--- simple/src/test/java/jack/mybatis/simple/mapper/UserMapperTest.java
+++ simple/src/test/java/jack/mybatis/simple/mapper/UserMapperTest.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Date;
 import java.util.List;
 
 public class UserMapperTest extends BaseMapperTest {
@@ -40,5 +41,115 @@ public class UserMapperTest extends BaseMapperTest {
             Assert.assertNotNull(userRoles);
             Assert.assertTrue(userRoles.size() > 0);
         }
+    }
+
+    @Test
+    public void testInsert() {
+        SqlSession sqlSession = this.getSqlSession();
+        try {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            SysUser user = createUser();
+            int result = mapper.insert(user);
+            Assert.assertEquals(1, result);
+            Assert.assertNull(user.getId());
+        }finally {
+            sqlSession.rollback();
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testInsert2() {
+        SqlSession sqlSession = this.getSqlSession();
+        try {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            SysUser user = createUser();
+            int result = mapper.insert2(user);
+            Assert.assertEquals(1, result);
+            Assert.assertNotNull(user.getId());
+
+            System.out.println(user.getId());
+        }finally {
+            sqlSession.rollback();
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testInsert3() {
+        SqlSession sqlSession = this.getSqlSession();
+        try {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            SysUser user = createUser();
+            int result = mapper.insert3(user);
+            Assert.assertEquals(1, result);
+            Assert.assertNotNull(user.getId());
+
+            System.out.println(user.getId());
+        }finally {
+            sqlSession.rollback();
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testUpdateById(){
+        SqlSession sqlSession = this.getSqlSession();
+        try{
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            SysUser user = mapper.selectById(1L);
+            Assert.assertEquals("admin", user.getUserName());
+            user.setUserName("admin_test");
+            user.setUserEmail("test@jack.com");
+
+            int result = mapper.updateById(user);
+            Assert.assertEquals(1, result);
+            user = mapper.selectById(1L);
+            Assert.assertEquals("admin_test", user.getUserName());
+        }finally {
+            sqlSession.rollback();
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testDeleteById(){
+        SqlSession sqlSession = this.getSqlSession();
+        try{
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            SysUser user = mapper.selectById(1L);
+            Assert.assertNotNull(user);
+
+            Assert.assertEquals(1, mapper.deleteById(user));
+            Assert.assertNull(mapper.selectById(1L));
+        }finally {
+            sqlSession.rollback();
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testSelectRolesByUserIdAndRoleEnabled(){
+        SqlSession sqlSession = this.getSqlSession();
+        try{
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            List<SysRole> roles = mapper.selectRolesByUserIdAndRoleEnabled(1L, 1);
+            Assert.assertNotNull(roles);
+            Assert.assertTrue(roles.size() > 0);
+        }finally {
+            sqlSession.close();
+        }
+    }
+
+    private SysUser createUser(){
+        SysUser user = new SysUser();
+        user.setUserName("jack");
+        user.setUserPassword("123456");
+        user.setUserEmail("test@mybatis.jack");
+        user.setUserInfo("test info");
+        user.setHeadImg(new byte[]{1, 2, 3});
+        user.setCreateTime(new Date());
+
+        return user;
     }
 }
